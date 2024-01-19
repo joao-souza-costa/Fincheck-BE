519d52d345671eba39fcc253b06d7d658bfe1ecf
Joao Souza <joao.souza@avenuecode.com>
2024-01-18 23:58:47 -0300

    fixing dash route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, RouterModule } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 import { HelpersModule } from './shared/helpers/helpers.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+import { AppModules } from './modules/app/app.module';
 
 @Module({
   imports: [
+    AppModules,
     UsersModule,
     DatabaseModule,
     AuthModule,
